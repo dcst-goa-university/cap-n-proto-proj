dd361b6aefe565a508a34b01d3f5a96004ed841d
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-12-23 12:59:22 +0530

    init python projs

---
+++ py/cnp-grpc-server/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cnp-grpc-server!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ py/cnp-iot-emitter/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cnp-iot-emitter!")
+
+
+if __name__ == "__main__":
+    main()
